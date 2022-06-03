99e3b00fe33267bef4f2c2fc091316db47518520
DSLProgramming <daniel.leykin@mcvts.org>
2022-06-02 21:32:20 -0400

    Post Vicky meeting, first prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
+
+const primaryColor = Color(0xFFffffff);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Jetlag',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primaryColor: primaryColor,
       ),
       home: const MyHomePage(title: 'Jetlag'),
     );
@@ -50,90 +51,106 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        toolbarHeight: 120,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: <Color>[
+                Color(0xFFf6f9d8),
+                Color(0xFF0e3b27),
+              ])),
+        ),
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-
-        title: Text(widget.title),
+        title: SizedBox(
+          height: 500.0,
+          width: 500,
+          child: Image.asset('images/jetlag.jpg'),
+        ),
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(12, 2, 12, 2),
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 15, 10, 5),
-              child: Image.asset('images/clearlogo.png.png'),
+      body: Scrollbar(
+        isAlwaysShown: true,
+        //lets you scroll
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(12, 2, 12, 2),
+            child: Center(
+              child:
+                  Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 15, 10, 5),
+                  child: Image.asset('images/tomLogo.jpg'),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 4, 0, 10),
+                  child: mainTitleText("\nJETLAG-2022 SERENDIPITY: NO WAR"),
+                ),
+                Text(
+                  "The JetLAG-2022 program will be focusing on the anti-war message and a fundraiser, although we wish and hope that Russia's aggression in Ukraine would stop by then. \nWe know that even once it’s over, a more continuous, global and vicious circle of war, violence, repressions, hatred, partitioning, division and displacement, will be still ongoing as it has always been. \nWe, therefore, see it as a time to contemplate the roots of a great tradition of open-air music festivals countering this evil with the wisdom, beauty and boldness of the liberal and transformational arts, striving for freedom, peace and justice, for a better world to come.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Lato',
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black,
+                    fontSize: 20,
+                    height: 2.0,
+                  ),
+                ),
+              ]),
             ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 4, 0, 10),
-              child: mainTitleText("\nJETLAG-2022 SERENDIPITY: NO WAR"),
-            ),
-            Text(
-              "The JetLAG-2022 program will be focusing on the anti-war message and a fundraiser, although we wish and hope that Russia's aggression in Ukraine would stop by then. \nWe know that even once it’s over, a more continuous, global and vicious circle of war, violence, repressions, hatred, partitioning, division and displacement, will be still ongoing as it has always been. \nWe, therefore, see it as a time to contemplate the roots of a great tradition of open-air music festivals countering this evil with the wisdom, beauty and boldness of the liberal and transformational arts, striving for freedom, peace and justice, for a better world to come.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'Lato',
-                fontWeight: FontWeight.w300,
-                color: Colors.black,
-                fontSize: 20,
-                height: 2.0,
-              ),
-            ),
-          ]),
+          ),
         ),
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.green,
-              ),
-              child: Center(
-                child: Image.asset('images/clearlogo.png.png'),
+            SizedBox(
+              height: 350.0,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color(0xFF000000),
+                ),
+                child: Center(
+                  child: Image.asset('images/regLogo.jpg'),
+                ),
               ),
             ),
             ListTile(
               leading: Icon(
                 Icons.music_note_rounded,
                 color: Colors.black,
-                size: 50,
+                size: 80,
               ),
               title: Align(
-                alignment: Alignment.centerLeft,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    textStyle: TextStyle(fontSize: 30),
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: 145,
+                  width: 200,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.blue,
+                        width: 4,
+                      ),
+                    ),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        textStyle: TextStyle(fontSize: 40),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Lineup()),
+                        );
+                      },
+                      child: getText('Lineup'),
+                    ),
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Lineup()),
-                    );
-                  },
-                  child: getText('Lineup'),
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.list_alt_rounded,
-                color: Colors.black,
-                size: 50,
-              ),
-              title: Align(
-                alignment: Alignment.centerLeft,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    textStyle: TextStyle(fontSize: 30),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Tickets()),
-                    );
-                  },
-                  child: getText('Tickets'),
                 ),
               ),
             ),
@@ -141,15 +158,23 @@ class _MyHomePageState extends State<MyHomePage> {
               leading: Icon(
                 Icons.map,
                 color: Colors.black,
-                size: 50,
+                size: 80,
               ),
-              title: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
+              title: Align(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: 145,
+                  width: 200,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.blue,
+                        width: 4,
+                      ),
+                    ),
                     child: TextButton(
                       style: TextButton.styleFrom(
-                        textStyle: TextStyle(fontSize: 30),
+                        textStyle: TextStyle(fontSize: 40),
                       ),
                       onPressed: () {
                         Navigator.push(
@@ -160,28 +185,40 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: getText('Map'),
                     ),
                   ),
-                ],
+                ),
               ),
             ),
             ListTile(
               leading: Icon(
                 Icons.question_mark_rounded,
                 color: Colors.black,
-                size: 50,
+                size: 80,
               ),
               title: Align(
-                alignment: Alignment.centerLeft,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    textStyle: TextStyle(fontSize: 30),
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: 145,
+                  width: 200,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.blue,
+                        width: 4,
+                      ),
+                    ),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        textStyle: TextStyle(fontSize: 30),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Info()),
+                        );
+                      },
+                      child: getText('Info'),
+                    ),
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Info()),
-                    );
-                  },
-                  child: getText('Info'),
                 ),
               ),
             ),
@@ -221,34 +258,13 @@ class Lineup extends StatelessWidget {
       appBar: AppBar(
         title: const Text('2022 Artist Lineup'),
       ),
+      body: Scrollbar(
+        isAlwaysShown: true,
 
-    );
-  }
-}
-
-class Tickets extends StatelessWidget {
-  const Tickets({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ticket'),
-      ),
-      body:
-      Scrollbar(
-      isAlwaysShown: true,
-    //lets you scroll
-    child: SingleChildScrollView(
-      child: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go back!'),
+        //lets you scroll
+        child: SingleChildScrollView(
+          child: Image.asset('images/bannerCrop.png'),
         ),
-      ),
-    ),
       ),
     );
   }
@@ -264,12 +280,17 @@ class Map extends StatelessWidget {
         title: const Text('Map'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go back!'),
+        child: Text(
+          'Google API Implementation/Custom map',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontWeight: FontWeight.w900,
+            color: Colors.black,
+            fontSize: 50,
+            height: 2.0,
+          ),
         ),
+        // Navigate back to first route when tapped.
       ),
     );
   }
@@ -282,14 +303,14 @@ class Info extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Infomation'),
+        title: const Text('Information'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go back!'),
+      body: Scrollbar(
+        isAlwaysShown: true,
+
+        //lets you scroll
+        child: SingleChildScrollView(
+          child: Image.asset('images/guide.png'),
         ),
       ),
     );
